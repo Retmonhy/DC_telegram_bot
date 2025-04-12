89979db2edda7c1994d681db70d345c9845bcb83
retmonhy <retmonhy@gmail.com>
2025-04-12 18:40:10 +0700

    Добавил вывод колиечства рассчетов в блоке Как это работает

--- source/classes/ModelStore.py
+++ source/classes/ModelStore.py
@@ -3,6 +3,7 @@ from enums import DarkRitualsAmount
 # хранит модели игроков(каждая модель рассчета связана с чатом)
 class ModelStore:
   model_dict: dict[int, dict] = {}
+  total_calculations: int = 0
 
   @staticmethod
   def add_model(key: int, model: DarkRitualsAmount):
@@ -15,3 +16,7 @@ class ModelStore:
   @staticmethod
   def delete_model_by_id(id: int):
     return ModelStore.model_dict.pop(id, None)
+  
+  @staticmethod
+  def increase_calc_counter():
+    ModelStore.total_calculations += 1
